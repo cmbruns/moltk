7798ddb296781bbf466b11d1a9c46c52623f078a
Christopher.Bruns@gmail.com <cmbruns@users.noreply.github.com>
2011-09-20 01:06:16 +0000

    Created stub for atom class

--- include/chem/Structure.h
+++ include/chem/Structure.h
@@ -2,13 +2,17 @@
 #define MOLTK_CHEM_PDB_STRUCTURE_H
 
 #include <iostream>
+#include <vector>
+#include "Atom.h"
 
 namespace moltk { namespace pdb {
 	
 class Structure
 {
 public:
-	Structure(std::istream&);
+    Structure(std::istream&);
+
+    std::vector<Atom> atoms;
 };
 	
 }}
