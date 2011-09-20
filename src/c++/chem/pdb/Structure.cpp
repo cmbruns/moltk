7d9cc5724b5045917e3c6f5c8e4e0a9f3db768d2
Christopher.Bruns@gmail.com <cmbruns@users.noreply.github.com>
2011-09-20 01:50:49 +0000

    Moved gzip decompression into Structure class.

--- src/c++/chem/pdb/Structure.cpp
+++ src/c++/chem/pdb/Structure.cpp
@@ -1,19 +1,61 @@
 #include "chem/Structure.h"
 #include <string>
+#include <fstream>
 #include <iostream>
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4996)
+#endif
+
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
 using namespace std;
+using namespace boost::iostreams;
 
 namespace moltk { namespace pdb {
 
+/* explicit */
+Structure::Structure(const std::string& fileName)
+{
+    loadFromFile(fileName);
+}
+
+/* explicit */
 Structure::Structure(std::istream& istream)
 {
     loadFromStream(istream);
 }
 
-/* explicit */
-void Structure::loadFromStream(std::istream& istream)
+bool Structure::loadFromFile(const std::string& fileName)
 {
+    ifstream pdbFile(fileName.c_str(), ios_base::in | ios_base::binary);
+    if (! pdbFile.good()) {
+        cerr << "Problem opening file " << fileName << endl;
+        return false;
+    }
+	filtering_streambuf<input> in;
+    if ( (fileName.substr(fileName.length() - 3) == ".gz") 
+      || (fileName.substr(fileName.length() - 3) == ".GZ") )
+    {
+    	in.push(gzip_decompressor());
+    }
+	in.push(pdbFile);
+    std::istream inStream(&in);
+    return loadFromStream(inStream);
+}
+
+/* explicit */
+bool Structure::loadFromStream(std::istream& istream)
+{
+    if (!istream.good())
+        return false;
     std::string currentLine;
     int atomCount = 0;
     while (istream.good()) 
@@ -26,6 +68,7 @@ void Structure::loadFromStream(std::istream& istream)
         }
     }
     cerr << atomCount << " atoms found." << endl;
+    return true;
 }
 
 }} // namespace moltk::pdb
--- test/test_load_pdb.cpp
+++ test/test_load_pdb.cpp
@@ -1,22 +1,6 @@
 #include "chem/pdb.h"
-#include <fstream>
-#include <iostream>
-
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable:4996)
-#endif
-
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
 
 using namespace std;
-using namespace boost::iostreams;
 using namespace moltk;
 
 int main(int argc, char* argv[])
@@ -25,22 +9,9 @@ int main(int argc, char* argv[])
         cerr << "File name argument missing" << endl;
         return 1; // file name argument missing
     }
-    const char* pdbFileName = argv[1];
-    ifstream pdbFile(pdbFileName, ios_base::in | ios_base::binary);
-    if (! pdbFile.good()) {
-        cerr << "Problem opening file " << pdbFileName << endl;
-        return 1;
-    }
-	filtering_streambuf<input> in;
-	in.push(gzip_decompressor());
-	in.push(pdbFile);
-    std::istream inStream(&in);
+    std::string pdbFileName(argv[1]);
 
-    cerr << "Printing file contents..." << endl;
-	// boost::iostreams::copy(in, cout);
-
-    pdb::Structure pdbStructure(inStream);
-    // TODO - parse pdb file
+    pdb::Structure pdbStructure(pdbFileName);
 
     return 0;
 }
