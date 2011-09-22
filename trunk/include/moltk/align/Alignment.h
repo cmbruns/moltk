f6d0f3528137de0a9269ffd153f3e638e378daaa
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@e2da44d5-31d0-08ba-c982-952e0c95ac53>
2011-09-22 01:31:21 +0000

    Start to work up dynamic programming alignment.

--- trunk/include/moltk/align/Alignment.h
+++ trunk/include/moltk/align/Alignment.h
@@ -3,13 +3,36 @@
 
 #include "Sequence.h"
 #include <iostream>
+#include <vector>
 
 namespace moltk {
+
+// TODO use boost::units for information
+typedef double Information;
 
 class Alignment {
 public:
     Alignment(const Sequence&, const Sequence&);
     void align();
+
+protected:
+    Information gapOpenPenalty;
+    Information gapExtensionPenalty;
+    bool bEndGapsFree;
+    bool bLocalAlignment;
+
+
+    class Cell
+    {
+        // Gusfield nomenclature
+        Information v;
+        Information g;
+        Information e;
+        Information f;
+    };
+
+
+    typedef std::vector<std::vector<Cell> > DpTable;
 };
 
 } // namespace moltk
