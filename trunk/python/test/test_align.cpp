5ab7973bed2b85601c5df1c3751e6a9ee09e685f
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@e2da44d5-31d0-08ba-c982-952e0c95ac53>
2011-09-30 13:47:04 +0000

    Started rearranging directories

---
+++ trunk/python/test/test_align.cpp
@@ -0,0 +1,18 @@
+#include "moltk/align.h"
+#include "moltk/fasta.h"
+#include <iostream>
+
+using namespace std;
+using namespace moltk::align;
+using namespace moltk;
+
+int main()
+{
+    FastaSequence seq1("AACDEF");
+    FastaSequence seq2("ACEFG");
+    Aligner aligner;
+    Alignment alignment = aligner.align(seq1, seq2);
+    cout << alignment;
+
+    return 0;
+}
