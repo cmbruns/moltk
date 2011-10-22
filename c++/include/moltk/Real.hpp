5ad384e15cdc3471dcf0dcb7b477008ff26691e8
ckbruns@gmail.com <ckbruns@users.noreply.github.com>
2011-10-22 01:01:13 +0000

    Updated license text.

--- c++/include/moltk/Real.hpp
+++ c++/include/moltk/Real.hpp
@@ -16,6 +16,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     
+    Commercial users should ask about our dual licensing model.
     For questions contact: cmbruns@rotatingpenguin.com
 */
 
