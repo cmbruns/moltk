fd6ce7706378379cf7371cf9d1574c6fa28a5503
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@e2da44d5-31d0-08ba-c982-952e0c95ac53>
2012-07-07 01:46:09 +0000

    Rudimentary display of pdb molecule   does not work with sphere imposters   reveals that something is wrong with rotation

--- trunk/python/moltk/gui/voltest/pyvolapp.py
+++ trunk/python/moltk/gui/voltest/pyvolapp.py
@@ -1,4 +1,5 @@
-from scenes import FiveBallScene, TeapotActor
+from rotation import Vec3
+from scenes import FiveBallScene, TeapotActor, SphereImposter, GlutSphereActor
 from pyvolrenderer import PyVolRenderer
 from pyvolcanvas import PyVolCanvas
 from pyvol_ui import Ui_MainWindow
@@ -13,7 +14,7 @@ class PyVolMainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-    @QtCore.Slot()        
+    @QtCore.Slot()
     def on_actionOpen_triggered(self):
         file_name, type = QFileDialog.getOpenFileName(
                 self, 
@@ -22,9 +23,23 @@ class PyVolMainWindow(QMainWindow):
                 self.tr("PDB Files(*.pdb)"))
         if file_name == "":
             return
+        atoms = []
         with open(file_name, 'r') as f:
             print file_name
-            pass
+            for line in f:
+                if line.startswith("ATOM"):
+                    x = float(line[30:38])
+                    y = float(line[38:46])
+                    z = float(line[46:54])
+                    atoms.append(Vec3([x,y,z]))
+        print len(atoms), "atoms found"
+        if len(atoms) > 0:
+            ren = self.ui.glCanvas.renderer
+            # ren.actors = []
+            for atom in atoms:
+                ren.actors.append(GlutSphereActor(atom, 1.2))
+            ren.actors.append(GlutSphereActor([0,0,0], 1.1))
+            ren.update()
 
 
 class PyVolApp(QApplication):
