b318cbf34bae9dfae0d337293a93b02dc478f60b
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@e2da44d5-31d0-08ba-c982-952e0c95ac53>
2012-07-08 22:14:04 +0000

    Wire up 3d menu items

--- trunk/python/moltk/gui/voltest/stereo3d.py
+++ trunk/python/moltk/gui/voltest/stereo3d.py
@@ -1,18 +1,19 @@
 
 class Mono:
-    def views(self, camera):
+    def set_defaults(self, camera):
         camera.viewport_xrelstart = 0.0
         camera.viewport_relwidth = 1.0
-        camera.eye_pos = 0.0 # middle eye
+        camera.eye_pos = 0.0 # middle eye        
+        
+    def views(self, camera):
+        self.set_defaults(camera)
         with camera:
             yield camera
 
 
 class Left(Mono):
     def views(self, camera):
-        # Left eye view on left side of viewport
-        camera.viewport_xrelstart = 0.0
-        camera.viewport_relwidth = 1.0
+        self.set_defaults(camera)
         camera.eye_pos = -1.0 # left eye
         with camera:
             yield camera
@@ -20,10 +21,8 @@ class Left(Mono):
 
 class Right(Mono):
     def views(self, camera):
-        # Left eye view on left side of viewport
-        camera.viewport_xrelstart = 0.0
-        camera.viewport_relwidth = 1.0
-        camera.eye_pos = -1.0 # left eye
+        self.set_defaults(camera)
+        camera.eye_pos = 1.0 # right eye
         with camera:
             yield camera
 
@@ -31,12 +30,14 @@ class Right(Mono):
 class LeftRight(Mono):
     def views(self, camera):
         # Left eye view on left side of viewport
+        self.set_defaults(camera)
         camera.viewport_xrelstart = 0.0
         camera.viewport_relwidth = 0.5
         camera.eye_pos = -1.0 # left eye
         with camera:
             yield camera
         # Right eye view on right side of viewport
+        self.set_defaults(camera)
         camera.viewport_xrelstart = 0.5
         camera.viewport_relwidth = 0.5
         camera.eye_pos = +1.0 # right eye
@@ -47,15 +48,32 @@ class LeftRight(Mono):
 class RightLeft(Mono):
     def views(self, camera):
         # Left eye view on left side of viewport
+        self.set_defaults(camera)
         camera.viewport_xrelstart = 0.5
         camera.viewport_relwidth = 0.5
         camera.eye_pos = -1.0 # left eye
         with camera:
             yield camera
         # Right eye view on right side of viewport
+        self.set_defaults(camera)
         camera.viewport_xrelstart = 0.0
         camera.viewport_relwidth = 0.5
         camera.eye_pos = +1.0 # right eye
         with camera:
             yield camera
+            
+
+class RedCyan(Mono):
+    def views(self, camera):
+        # Left eye view red
+        self.set_defaults(camera)
+        camera.eye_pos = -1.0 # left eye
+        with camera:
+            yield camera
+        # Right eye view cyan
+        self.set_defaults(camera)
+        camera.eye_pos = +1.0 # right eye
+        with camera:
+            yield camera
+
 
