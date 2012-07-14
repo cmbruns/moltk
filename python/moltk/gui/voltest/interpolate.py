adf32e9946f71d8e02cee20df6d3cae522f68810
Christopher.Bruns@gmail.com <cmbruns@users.noreply.github.com>
2012-07-14 17:14:36 +0000

    Added rotation interpolation to movie making. Fixed a bug where cached movie parameters are not cleared with the key frames.

--- python/moltk/gui/voltest/interpolate.py
+++ python/moltk/gui/voltest/interpolate.py
@@ -1,13 +1,13 @@
 from math import floor
 
 class CatmullRomSpline:
-    def interpolate_sequence(self, sequence, index, loop=False):
+    def _compute_sequence_indices(self, sequence, index, doLoop):
         vals = []
         i1 = int(floor(index))
         for di in [-1, 0, 1, 2]:
             i = i1 + di
             # ensure index is within range
-            if loop: # interpolate between final and first elements
+            if doLoop: # interpolate between final and first elements
                 while i < 0:
                     i += len(sequence)
                 while i >= (len(sequence)):
@@ -18,7 +18,17 @@ class CatmullRomSpline:
                 if i >= len(sequence):
                     i = len(sequence) - 1
             vals.append(sequence[i])
+        return vals
+    
+    def interpolate_sequence(self, sequence, index, doLoop=False):
+        vals = self._compute_sequence_indices(sequence, index, doLoop)
+        i1 = int(floor(index))
         return self.interpolate(vals[0], vals[1], vals[2], vals[3], float(index) - i1)
+    
+    def interpolate_quaternion_sequence(self, sequence, index, doLoop=False):
+        vals = self._compute_sequence_indices(sequence, index, doLoop)
+        i1 = int(floor(index))
+        return self.interpolate_quaternion(vals[0], vals[1], vals[2], vals[3], float(index) - i1)
     
     def interpolate(self, p0, p1, p2, p3, t):
         # from http://www.mvps.org/directx/articles/catmull/
@@ -28,4 +38,16 @@ class CatmullRomSpline:
                    + (-p0 + p2) * t
                    + (2.0*p0 - 5.0*p1 + 4.0*p2 - p3) * t2
                    + (-p0 + 3.0*(p1 - p2) + p3) * t3 )
+        
+    def interpolate_quaternion(self, q00, q01, q02, q03, t):
+        """
+        From method CatmullQuat(...) on page 449 of Visualizing Quaternions
+        """
+        q10 = q00.slerp(q01, t+1.0)
+        q11 = q01.slerp(q02, t+0.0)
+        q12 = q02.slerp(q03, t-1.0)
+        q20 = q10.slerp(q11, (t+1.0)/2.0)
+        q21 = q11.slerp(q12, t/2.0)
+        return q20.slerp(q21, t)
+        
         
